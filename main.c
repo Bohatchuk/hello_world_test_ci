ccab03ad71255288b6ad38ad0fa670b359852754
Daria Bohatchuk <dariab@mellanox.com>
2020-01-31 15:41:30 +0000

    check please
    Signed-off-by: Daria Bohatchuk <dariab@mellanox.com>

--- main.c
+++ main.c
@@ -12,6 +12,6 @@ int main()
     printf("are you working?\n");
     printf("i'm so tired......\n");
     printf("want to find the issue\n");
-    printf("check\n");
+    printf("check please\n");
     return 0;
 }
