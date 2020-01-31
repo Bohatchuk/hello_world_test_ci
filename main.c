479cf690cf6d3fd8c695e269a5bae50de1620c30
Daria Bohatchuk <dariab@mellanox.com>
2020-01-31 15:48:18 +0000

    i believe i can touch the sky
    Signed-off-by: Daria Bohatchuk <dariab@mellanox.com>

--- main.c
+++ main.c
@@ -14,5 +14,6 @@ int main()
     printf("want to find the issue\n");
     printf("check please\n");
     printf("i believe i can fly\n");
+    printf("i believe i can touch the sky\n");
     return 0;
 }
