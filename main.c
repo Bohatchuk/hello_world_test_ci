9b46601c6ab1fe041c2548ef79ed1f7aa9fdd4dc
Daria Bohatchuk <dariab@mellanox.com>
2020-02-06 16:22:18 +0000

    delete one line
    Signed-off-by: Daria Bohatchuk <dariab@mellanox.com>

--- main.c
+++ main.c
@@ -4,7 +4,6 @@ int main()
 {
     printf("Hello world!\n");
     printf("One more line\n");
-    printf("One more line\n");
     printf("Add one more\n");
     printf("So, is it work properly?\n");
     printf("let's check votes\n");
